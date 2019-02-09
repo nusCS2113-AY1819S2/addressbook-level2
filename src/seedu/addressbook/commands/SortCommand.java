c39d0c869cd0591ddef1ef1d5ffea3c72c669718
Sean Yap <e0177170@u.nus.edu>
2019-02-10 06:32:34 +0800

    Fixed another typo in the Sort command class

--- src/seedu/addressbook/commands/SortCommand.java
+++ src/seedu/addressbook/commands/SortCommand.java
@@ -9,7 +9,7 @@ public class SortCommand extends Command {
     public static final String COMMAND_WORD = "sort";
 
     public static final String MESSAGE_USAGE = COMMAND_WORD + ":\n" 
-            + "Sort all persons in the address book in ascending order.\n\t"
+            + "Sort all persons in the address book in ascending order.\n"
             + "Example: " + COMMAND_WORD;
 
     public static final String MESSAGE_SUCCESS = "Address book has been sorted by ascending order!";
