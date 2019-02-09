9bb0223400c1e150b4f86fd46ea7652cb38486c6
lycjackie <leeyc@comp.nus.edu.sg>
2019-02-09 23:16:19 +0800

    Add SortCommandTest with 4 JUnits tests
    Slight modification made to TypicalPersons class to fit tests

---
+++ test/java/seedu/addressbook/commands/SortCommandTest.java
@@ -0,0 +1,60 @@
+package seedu.addressbook.commands;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import seedu.addressbook.data.AddressBook;
+import seedu.addressbook.data.exception.IllegalValueException;
+import seedu.addressbook.data.person.Person;
+import seedu.addressbook.data.person.ReadOnlyPerson;
+import seedu.addressbook.util.TestUtil;
+import seedu.addressbook.util.TypicalPersons;
+
+public class SortCommandTest {
+    private static final List<ReadOnlyPerson> EMPTY_PERSON_LIST = Collections.emptyList();
+    private TypicalPersons td = new TypicalPersons();
+    private List<ReadOnlyPerson> sortedListWithAllTypicalPerson = Arrays.asList(td.getTypicalPersons());
+    private List<ReadOnlyPerson> listWithSinglePerson = Arrays.asList(td.dan);
+    private List<ReadOnlyPerson> unsortedListWithAllypicalPersons = Arrays.asList(td.candy, td.dan, td.amy, td.bill);
+
+    /**
+     * Executes the sort command for the given list and verifies it is sorted
+     * the result matches the persons in the expectedPersons exactly in the same orders.
+     */
+
+    private static void assertSortBehavior(List<ReadOnlyPerson> inputPersons,
+                                           List<ReadOnlyPerson> expectedPersons) {
+        // create an addressbook via a new list
+        AddressBook addressBook = TestUtil.createAddressBook(
+                inputPersons.toArray(new Person[0]));
+
+        SortCommand command = new SortCommand();
+        command.setData(addressBook, EMPTY_PERSON_LIST);
+
+        // addressBook is now sorted
+        CommandResult result = command.execute();
+
+        assertEquals(addressBook.getAllPersons().immutableListView(), expectedPersons);
+    }
+
+    @Test
+    public void execute() throws IllegalValueException {
+
+        // empty list: matches
+        assertSortBehavior(EMPTY_PERSON_LIST, EMPTY_PERSON_LIST);
+
+        // single item list: matches
+        assertSortBehavior(listWithSinglePerson, listWithSinglePerson);
+
+        //same name but different order: matches
+        assertSortBehavior(unsortedListWithAllypicalPersons, sortedListWithAllTypicalPerson);
+
+        // Dan Grey follow by Dan Smiths: matches
+        assertSortBehavior(Arrays.asList(td.dan, td.candy, td.danGrey),
+                Arrays.asList(td.candy, td.danGrey, td.dan));
+    }
+}
--- test/java/seedu/addressbook/util/TypicalPersons.java
+++ test/java/seedu/addressbook/util/TypicalPersons.java
@@ -16,7 +16,7 @@ import seedu.addressbook.data.tag.Tag;
  */
 public class TypicalPersons {
 
-    public Person amy, bill, candy, dan;
+    public Person amy, bill, candy, dan, danGrey;
 
     public TypicalPersons() {
         try {
@@ -27,6 +27,8 @@ public class TypicalPersons {
             candy = new Person(new Name("Candy Destiny"), new Phone("93339333", true),
                     new Email("cd@gmail.com", false), new Address("3 Clementi Road", true), Collections.emptySet());
             dan = new Person(new Name("Dan Smith"), new Phone("1234556", true), new Email("ss@tt.com", true),
+                    new Address("NUS", true), Collections.singleton(new Tag("test")));
+            danGrey = new Person(new Name("Dan Grey"), new Phone("1234556", true), new Email("ss@tt.com", true),
                     new Address("NUS", true), Collections.singleton(new Tag("test")));
         } catch (IllegalValueException e) {
             e.printStackTrace();
