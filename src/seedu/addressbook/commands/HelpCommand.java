1be8cdde5dabf77497d33429072aceb39dd9fe2d
Creastery <Creastery@users.noreply.github.com>
2019-02-11 05:41:22 +0800

    commands: implement `sort` command
    We are ready to implement the `sort` command.
    
    The `sort` command helps to sort all persons in the address book by
    their names lexicographically. The command should be handled by the
    parser, and display the sample command usage in the `help` command so as
    to prevent confusion among users.
    
    Let's,
    * add a SortCommand class that calls AddressBook#sort
    * handle the SortCommand#COMMAND_WORD in Parser class
    * update the command usage in HelpCommand class

--- src/seedu/addressbook/commands/HelpCommand.java
+++ src/seedu/addressbook/commands/HelpCommand.java
@@ -17,6 +17,7 @@ public class HelpCommand extends Command {
                 AddCommand.MESSAGE_USAGE
                 + "\n" + DeleteCommand.MESSAGE_USAGE
                 + "\n" + ClearCommand.MESSAGE_USAGE
+                + "\n" + SortCommand.MESSAGE_USAGE
                 + "\n" + FindCommand.MESSAGE_USAGE
                 + "\n" + ListCommand.MESSAGE_USAGE
                 + "\n" + ViewCommand.MESSAGE_USAGE
---
+++ src/seedu/addressbook/commands/SortCommand.java
@@ -0,0 +1,20 @@
+package seedu.addressbook.commands;
+
+/**
+ * Sort all persons in the address book by their names lexicographically.
+ */
+public class SortCommand extends Command {
+
+    public static final String COMMAND_WORD = "sort";
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Sorts and re-orders all persons in the "
+            + "address book by their names lexicographically.\n"
+            + "Example: " + COMMAND_WORD;
+
+    public static final String MESSAGE_SUCCESS = "Address book has been sorted!";
+
+    @Override
+    public CommandResult execute() {
+        addressBook.sort();
+        return new CommandResult(MESSAGE_SUCCESS);
+    }
+}
