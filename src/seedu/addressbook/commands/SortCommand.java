52e7312eaa98758c9aaf4836d3488e71594e5453
Yeo Cheng Hong <yeo.ch@u.nus.edu>
2019-02-10 16:21:50 +0800

    src/seedu/addressbook/commands: Added SortCommand class

---
+++ src/seedu/addressbook/commands/SortCommand.java
@@ -0,0 +1,26 @@
+package seedu.addressbook.commands;
+
+import seedu.addressbook.data.person.ReadOnlyPerson;
+
+import java.util.List;
+
+
+/**
+ * Lists all persons in the address book to the user in a sorted order from capital [A-Z] to lower capital [a-z].
+ */
+public class SortCommand extends Command {
+
+    public static final String COMMAND_WORD = "sort";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD
+            + ": Displays all persons in the address book as a list with index numbers\n"
+            + " in a sorted order from capital [A-Z] to lower capital [a-z]\n"
+            + "Example: " + COMMAND_WORD;
+
+
+    @Override
+    public CommandResult execute() {
+        List<ReadOnlyPerson> allPersons = addressBook.getAllPersons().immutableListView();
+        return new CommandResult(getMessageForPersonListShownSummary(allPersons), allPersons);
+    }
+}
