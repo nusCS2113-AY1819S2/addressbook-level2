1d718409e6ac79457914b4504a54a3c28c3beba5
Creastery <Creastery@users.noreply.github.com>
2019-02-11 05:26:12 +0800

    AddressBook: add `sort` method
    The AddressBook class does not implement any methods that can be used to
    sort its `allPersons` UniquePersonList object.
    
    As a progressive step to implement the `sort` command, let's add a
    `sort` method to the AddressBook class to call UniquePersonList#sort.

--- src/seedu/addressbook/data/AddressBook.java
+++ src/seedu/addressbook/data/AddressBook.java
@@ -62,6 +62,13 @@ public class AddressBook {
     }
 
     /**
+     * Sort all persons in the address book by their names lexicographically.
+     */
+    public void sort() {
+        allPersons.sort();
+    }
+
+    /**
      * Returns a new UniquePersonList of all persons in the address book at the time of the call.
      */
     public UniquePersonList getAllPersons() {
