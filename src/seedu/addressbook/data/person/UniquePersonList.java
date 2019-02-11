428aa87ee8324d732718b39e342e1eed31d406e4
weizhang05 <chanweizhang05@hotmail.com>
2019-02-11 22:13:43 +0800

    changed to specific libraries import instead of general import

--- src/seedu/addressbook/data/person/UniquePersonList.java
+++ src/seedu/addressbook/data/person/UniquePersonList.java
@@ -1,6 +1,5 @@
 package seedu.addressbook.data.person;
 
-<<<<<<< HEAD
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
@@ -8,9 +7,6 @@ import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Comparator;
-=======
-import java.util.*;
->>>>>>> 010a1aaa2f24e3be95cfde8e8313f97fed16a8eb
 
 import seedu.addressbook.common.Utils;
 import seedu.addressbook.data.exception.DuplicateDataException;
