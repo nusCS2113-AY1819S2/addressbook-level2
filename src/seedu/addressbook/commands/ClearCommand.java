f06fd8f379ae7c9ab08a6d5126afb788e5aa51ed
Quinn <amourmq@gmail.com>
2019-02-11 13:52:12 +0800

    W5T0801-ZhaoJunRu Warning Message added

--- src/seedu/addressbook/commands/ClearCommand.java
+++ src/seedu/addressbook/commands/ClearCommand.java
@@ -1,4 +1,5 @@
 package seedu.addressbook.commands;
+import java.util.*;
 
 /**
  * Clears the address book.
@@ -8,12 +9,21 @@ public class ClearCommand extends Command {
     public static final String COMMAND_WORD = "clear";
     public static final String MESSAGE_USAGE = "Clears address book permanently.\n"
             + "Example: " + COMMAND_WORD;
-
     public static final String MESSAGE_SUCCESS = "Address book has been cleared!";
+    public static final String MESSAGE_FAILURE = "Exiting clear function";
+
 
     @Override
     public CommandResult execute() {
-        addressBook.clear();
-        return new CommandResult(MESSAGE_SUCCESS);
+        System.out.println("Confirm to clear the entire address book? YES to continue");
+        Scanner scanner = new Scanner(System.in);
+        String next = scanner.next();
+        if (next.equals("YES")) {
+            addressBook.clear();
+            return new CommandResult(MESSAGE_SUCCESS);
+        }
+
+        return new CommandResult(MESSAGE_FAILURE);
     }
 }
+
